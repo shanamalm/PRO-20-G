0d8aaa5ec146378b10578ee94d1bd9b3c6cd06a3
shanamalm <75960052+shanamalm@users.noreply.github.com>
2021-02-03 10:17:28 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,72 +1,47 @@
-var car1,wall1;
-var car2,wall;
+var car,wall;
+var car,wall;
 var speed,weight;
 
 function setup() {
-  createCanvas(1600,400);
+  createCanvas(400,400);
+  car=createSprite(50,200,20,10);
+ /// car.velocityX = random(20,30);
+  car.shapeColor = "red";
+
+
+  wall=createSprite(380,200,20,200)
+  wall.shapeColor="white";
 
   speed=random(55,90);
-  speed=random(400,1500);
+car.velocityX=speed
+  weight=random(400,1500);
 
-  car=createSprite(100,50,50,10);
-  car.shapeColor = "red";
-  
-  wall=createSprite(1500,50,20,height/9);
-  wall.shapeColor = "80,80,80";
-  
-  car2=createSprite(100,200,50,10);
-  car2.shapeColor = "green";
-  
-  wall=createSprite(1500,200,20,height/9);
-  wall.shapeColor = "80,80,80";
-  
 }
 
 function draw() {
-  background(0,0,0);  
- car.velocityX=5;
- car2.velocityX=5;
+  background("black");  
   
- if (wall.x - car.x < (car.width + wall.width)/2)
-  {
+  if (wall.x-car.x<(car.width+wall.width)/2){
     car.velocityX=0;
-    var deformation=0.5 * weight * speed*speed/22509;
+    var deformation=0.5*weight*speed*speed/22509;
+    if(deformation>180)
+    {
+      car.shapeColor=color(255,0,0);
+    }
+  
+    if(deformation<180 &&deformation>100){
+      car.shapeColor=color(230,230,0)
+    }
+  
+    if(deformation<100)
+    {
+      car.shapeColor=color(0,255,0);
+    }
 
-if(deformation>180)
-{
-  car.shapeColor=Color(225,0,0);
+  
+  
 }
-
-if(deformation<180 && deformation>100)
-{
-  car.shapeColor=Color(230,230,0);
-}
-
-if(deformation<100)
-{
-  car.shapeColor=Color(0,255,0);
-}
-}
-
-  if (wall.x - car2.x < (car2.width + wall.width)/2)
-  {
-    car2.velocityX=0;
-    var deformation=0.5 * weight * speed*speed/22509;
-
-if(deformation>180)
-{
-  car2.shapeColor=Color(225,0,0);
-}
-
-if(deformation<180 && deformation>100)
-{
-  car2.shapeColor=Color(230,230,0);
-}
-
-if(deformation<100)
-{
-  car2.shapeColor=Color(0,255,0);
-}
-}
+  
+  
   drawSprites();
 }
